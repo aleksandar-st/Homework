fb04fa63ad9c737fc623724b7a85ae3db8e84b5b
Aleksandar <aleksandars.st@gmail.com>
2014-09-20 19:48:47 +0300

    Add responsive.css

--- Web/RAPID TRUCK/scripts/main.js
+++ Web/RAPID TRUCK/scripts/main.js
@@ -20,6 +20,20 @@ $(document).ready(function() {
         var nameAttr=$(this).attr("name");
         $("."+nameAttr).slideUp(200)
     });
+
+    $(".toggle-nav").click( function(event){
+        event.preventDefault();
+        if ($(this).hasClass("isDown") ) {
+            $(".user-nav").removeClass("openNav", 1000); 
+            $(".main").removeClass("openBody", 1000);
+            $(this).removeClass("isDown");                    
+        } else {
+            $(".user-nav").addClass("openNav", 1000);   
+            $(this).addClass("isDown");
+            $(".main").addClass("openBody", 1000);
+        }
+        return false;
+    });
 }); 
 
 //This function return filename
