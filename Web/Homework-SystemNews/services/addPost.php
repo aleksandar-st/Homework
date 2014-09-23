a714db6931165aa3bb4505a6e51c47593bbb479a
Aleksandar <aleksandars.st@gmail.com>
2014-09-23 16:17:43 +0300

    Delete addPost.php

--- Web/Homework-SystemNews/services/addPost.php
+++
@@ -1,9 +0,0 @@
-<?php
-    require 'dbConfig.php';
-	$time=time();
-    $title=htmlspecialchars($_POST['title']);
-    $content=strip_tags($_POST['content']);
-    $author=$_POST['author'];
-    $q="INSERT INTO posts (title, content, author, added) VALUES ('$title','$content','$author','$time')";
-    mysql_query($q) or die(mysql_error());
-?>
